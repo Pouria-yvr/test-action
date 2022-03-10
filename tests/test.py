58d8f43e8b6f06a4bee661108b76111a65b10c00
Pouria  Lotfi <pouria@flexfitnessapp.com>
2022-03-10 09:07:49 -0800

    updated

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 class Testing(TestCase):
     def test_string(self):
         a = 'some'
-        b = 'some'
+        b = 'somew'
         self.assertEqual(a, b)
 
     def test_boolean(self):
